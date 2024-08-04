0ad4e02d2f4e0ca383380e3d34c89aadad953ba8
napharsh <harsh.goel@napworks.in>
2024-08-04 16:39:53 +0530

    routes change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import './App.css'
-import { Route, Routes } from "react-router-dom";
+import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import { useSelector } from 'react-redux';
+
 function App() {
-  const {token} = useSelector(state=>state.auth)
+  const { token } = useSelector(state => state.auth);
+
   return (
     <Routes>
-      {
-        token ? 
-        (  <Route path="/" element={<Home/>}/>) :
-        (  <Route path="/login" element={<Login/>}/>)
-      }
-      <Route path="/signup" element={<Signup/>}/>
-    
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
+      <Route path="*" element={<Navigate to={token ? "/" : "/login"} />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
